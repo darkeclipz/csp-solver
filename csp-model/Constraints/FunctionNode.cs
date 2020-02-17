7ef7410dd5caba5093952acd38790e42f7b85387
darkeclipz <larsrotgers@gmail.com>
2020-02-17 23:25:40 +0100

    FunctionNode

---
+++ csp-model/Constraints/FunctionNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csp.Model.Constraints
+{
+    public class FunctionNode : ConstraintNode
+    {
+        private Func<float> function;
+
+        public FunctionNode(Func<float> func /* variables .... */)
+        {
+            function = func;
+        }
+
+        public override int Evaluate()
+        {
+            return 0;
+        }
+    }
+}
